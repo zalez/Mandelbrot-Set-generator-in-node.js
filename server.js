bdd1af64ffc76891ea2327781efae75a9bb4aeaa
Constantin Gonzalez <constantin@glez.de>
2011-04-11 15:37:15 +0200

    Rewriting server to use connect.

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 /*
  * server.js
  *
- * A simple HTTP server in node, used to dispatch to more interesting scripts.
+ * A simple connect HTTP server, used to dispatch to more interesting functions.
  *
  */
 
 // Modules we want to use.
-var http = require('http');
-var url = require('url');
+var connect = require('connect');
+
+// Our self-written modules.
 var simplemandelbrot = require('simplemandelbrot.js');
 
 function do_simplemandelbrot(req, res) {
@@ -16,17 +17,7 @@ function do_simplemandelbrot(req, res) {
 }
 
 // Simple HTTP server with some dispatch logic.
-var server = http.createServer(function (req, res) {
-  requrl = url.parse(req.url);
-  switch(requrl.pathname) {
-    case '/':
-    case '/simplemandelbrot':
-      do_simplemandelbrot(req, res);
-    default:
-      res.writeHead(200, { "Content-Type": "text/plain" })
-      res.end("Sorry, can't find path \"" + requrl.pathname + "\".");
-  }
-});
- 
-server.listen(process.env.PORT || 8001);
-
+var server = connect.createServer(
+  connect.favicon();
+  connect.logger();
+);

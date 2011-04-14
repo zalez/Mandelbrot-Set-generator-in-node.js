bb641a651cae0a1945187c88838387040eb9edc7
Constantin Gonzalez <constantin@glez.de>
2011-04-14 22:43:09 +0200

    Separating colormap logic.

--- colormap.js
+++ colormap.js
@@ -11,11 +11,11 @@ exports.colormap = function (size) {
 
   for (var i = 0; i < size; i++) {
     j = i / size;
-    colormap[i] = {[
+    colormap[i] = [
       Math.floor((Math.sin(j * 2 * Math.PI) + 1) * 255 / 2),
       Math.floor((Math.sin(j * 3 * Math.PI) + 1) * 255 / 2),
       Math.floor((Math.sin(j * 4 * Math.PI) + 1) * 255 / 2)
-    ]};
+    ];
   }
 
   // The entry for 0 is always black

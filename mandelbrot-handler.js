589c1579b411fddb37079c189daddab940b2c085
Constantin Gonzalez <constantin@glez.de>
2011-04-18 13:23:29 +0200

    Changing to Cairo as a graphics library.

--- mandelbrot-handler.js
+++ mandelbrot-handler.js
@@ -23,19 +23,12 @@ const COLORS = MAX_ITER * 10;
 const OPT = 3; // Whether to use the optimized subseparation algorithm
 const MAX_OPT = 4;
 
-// The amount of scale to use. This multiplies the resolution we use to render, in order
-// to improve antialiasing.
-// The size is multiplied with this number when rendering the image, then the image is shrunk
-// by this factor to produce the end result.
-const RENDERSCALE = 2;
-
 // Modules we want to use.
 var connect = require('connect');
 var mandelbrot = require('mandelbrot-engine.js');
 var colormap = require('colormap.js');
 
-// var png = require ('png').Png;
-var Canvas = require('canvas');
+var png = require ('png').Png;
 
 var url = require ('url')
 
@@ -63,24 +56,14 @@ function show_image(req, res) {
   if (opt < 0) opt = OPT;
   if (opt > MAX_OPT) opt = OPT;
 
-  var rendersize = size * RENDERSCALE;
-
   // Render a Mandelbrot set into a result array
-  var result = mandelbrot.render(rendersize, RE_CENTER, IM_CENTER, ppu, max, opt);
+  var result = mandelbrot.render(size, RE_CENTER, IM_CENTER, ppu, max, opt);
 
   // Create a colormap.
   var map = colormap.colormap(COLORS);
 
   // Create an image buffer.
-  // var image = new Buffer(size * size * 3);
-
-  // Create an image canvas. Bigger than the image we're supposed to create, so we
-  // Get some antialiasing.
-  var canvas = new Canvas(rendersize, rendersize);
-  var ctx = canvas.getContext('2d');
-  ctx.clearRect(0,0,rendersize,rendersize);
-
-  var image = canvas.data;
+  var image = new Buffer(size * size * 3);
 
   // Fill the image buffer with the result from the Mandelbrot set, mapped to the colormap.
   var pos = 0;
@@ -88,21 +71,18 @@ function show_image(req, res) {
   for (i = 0; i < rendersize * rendersize; i++) {
     index=Math.floor(result[i]*COLORS);
     color = map[index];
-    image[pos++] = 255;
     image[pos++] = color[0];
     image[pos++] = color[1];
     image[pos++] = color[2];
   }
 
-  // Rescale the image to the output size.
-  ctx.scale(1/RENDERSCALE, 1/RENDERSCALE);
-
   // Convert the image into PNG format.
-  var png_file = canvas.toBuffer();
+  var png_image = new Png(image, size, size, 'rgb');
+  var png_file = png_image.encodeSync();
 
   // Return the image to the browser.
   res.writeHead(200, { "Content-Type": "image/png" });
-  res.end(png_file, 'binary');
+  res.end(png_file.toString(), 'binary');
 }
 
 exports.handler = connect.router(function(app) {

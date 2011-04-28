35cbddc9a5635453a5afb33c21bc63ffe942c593
Constantin Gonzalez <constantin@glez.de>
2011-04-28 16:29:22 +0200

    Debugging user detection in server.js.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var server = connect.createServer(
 server.use('/simplemandelbrot', simplemandelbrot.handler);
 server.use('/mandelbrot', mandelbrot.handler);
 
-process.stdout.write("User: " + process.env.USER + "\n");
+process.stdout.write("User: " + process.env + "\n");
 
 if (process.env.USER == "node") {
   server.listen(80);

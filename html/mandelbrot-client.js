6a94cbf870cddbf84aba8235fa58647ff5d1d69d
Constantin Gonzalez <constantin@glez.de>
2011-05-01 12:56:21 +0200

    Creating Mandelbrot client JavaScript

--- html/mandelbrot-client.js
+++ html/mandelbrot-client.js
@@ -26,14 +26,47 @@ var image;    // The <img> element that contains the Mandelbrot image.
 var controls; // The <div> element that containes the controls.
 var model;    // The Mandelbrot view configuration.
 
+// Update the mandelbrot image with a new set of parameters.
+function updateImage() {
+  var url = imageBaseURL;
+
+  // Convert all keys in our model to URL parameters.
+  for (var key in model) {
+    url += key + "=" + model[key] + "&";
+  }
+
+  // Truncate the last & if we added parameters.
+  if (url != imageBaseURL) {
+    url = url.substring(0, url.length - 1);
+  }
+
+  // Update the URL of the image. This will trigger a reload.
+  image.src = url;
+
+  return;
+}
+
+// Create the DOM elements needed to display the image.
 function createImage() {
   image = document.createElement("img");
 
   image.alt = "The Mandelbrot Set";
   document.getElementById(imageDivId).appendChild(image);
   updateImage();
+
+  return;
 }
 
+// Update the anti-aliasing settings and trigger an update of the image.
+function updateAA(option) {
+  if (model.aa != option.value) {
+    model.aa = option.value;
+    updateImage();
+  }
+  return;
+}
+
+// Create the DOM elements needed for the controls.
 function createControls() {
   var form = document.createElement("form");
   var aaSelection,aaOption;
@@ -45,6 +78,7 @@ function createControls() {
   for (var i = 0; i < aaValues.length; i++) {
     aaOption = document.createElement("option");
     aaOption.value = i;
+    aaOption.onchange = "updateAA(this);"
     aaOption.appendChild(document.createTextNode(aaValues[i]));
     aaSelection.appendChild(aaOption);
   }
@@ -53,6 +87,7 @@ function createControls() {
   controls.appendChild(form);
 }
 
+// Initialize everything.
 function init() {
   model = defaultModel;
 
@@ -60,20 +95,9 @@ function init() {
   createControls();
 }
 
-function updateImage() {
-  var url = imageBaseURL;
-
-  for (var key in model) {
-    url += key + "=" + model[key] + "&";
-  }
-
-  // Truncate the last & if we added parameters.
-  if (url != imageBaseURL) {
-    url = url.substring(0, url.length - 1);
-  }
-
-  image.src = url;
-}
-
+// This writes two div tags at the place this script was loaded. They'll be filled
+// with the Mandelbrot image and its controls.
 document.write("<div id=\"" + imageDivId + "\"></div><div id=\"" + controlsDivId + "\"></div>\n");
+
+// Initialize.
 init();

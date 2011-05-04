6024b45019d15315ec1fe78b89aea456e2757ba8
Constantin Gonzalez <constantin@glez.de>
2011-05-04 21:43:08 +0200

    Reimplementing selective optimization.

--- html/mandelbrot-client.js
+++ html/mandelbrot-client.js
@@ -124,7 +124,7 @@ function newPosition(e) {
   var y = e.pageY + imageXY[1];
 
   model.re = model.re - (model.size/model.ppu)/2 + x / model.ppu;
-  model.im = model.im - (model.size/model.ppu)/2 + y / model.ppu;
+  model.im = model.im + (model.size/model.ppu)/2 - y / model.ppu;
 
   updateImage();
 

87adf8d798ba71de372b5963ec4eecdef7bdbbec
Constantin Gonzalez <constantin@glez.de>
2011-05-10 17:37:51 +0200

    Re-implementing adaptive optimization.

--- mandelbrot-engine.js
+++ mandelbrot-engine.js
@@ -705,7 +705,7 @@ function subdivide_quadratic(set, iterator) {
     if (set.image.sx % subsize) {
       subdivide_quadratic(set.subimage(x, set.image.y, set.image.sx % subsize, set.image.sy), iterator);
     } else { // If there's no rest, then there's still one more left to render.
-      //render_opt(set.subimage(x, set.image.y, subsize, subsize), iterator);
+      render_opt(set.subimage(x, set.image.y, subsize, subsize), iterator);
     }
     return;
   } else { // taller than wide
@@ -718,7 +718,7 @@ function subdivide_quadratic(set, iterator) {
     if (set.image.sy % subsize) {
       subdivide_quadratic(set.subimage(set.image.x, y, set.image.sx, set.image.sy % subsize), iterator);
     } else {
-      //render_opt(set.subimage(set.image.x, y, subsize, subsize), iterator);
+      render_opt(set.subimage(set.image.x, y, subsize, subsize), iterator);
     }
     return;
   }

b30d031c0ffd0e913dfba6e4cf0754fa7c742772
Constantin Gonzalez <constantin@glez.de>
2011-05-10 10:51:57 +0200

    Re-implementing adaptive optimization.

--- mandelbrot-engine.js
+++ mandelbrot-engine.js
@@ -736,24 +736,24 @@ function render_adaptive(set) {
 
   // Render the top part until +1.2i with minimal optimization, as it's very easy anyway.
   newset = set.intersect(null, null, null, 1.2);
-  //if (newset.image.sy > 0) {
-  //  todo.push({
-  //    set: newset,
-  //    method: "basic",
-  //    iterator: iterate_basic
-  //  });
-  //}
+  if (newset.image.sy > 0) {
+    todo.push({
+      set: newset,
+      method: "basic",
+      iterator: iterate_basic
+    });
+  }
 
   // Use more optimization for the part between 1.2i and 0.75i. No need to test for bulbs, though.
   // Use the brain-dead algorithm for everything < 0.75 (re) and the subdivision algorithm for the right part.
   newset = set.intersect(null, 1.2, -0.75, 0.75);
-  //if (newset.image.sx > 0 && newset.image.sy > 0) {
-  //  todo.push({
-  //    set: newset,
-  //    method: "basic",
-  //    iterator: iterate_basic
-  //  });
-  //}
+  if (newset.image.sx > 0 && newset.image.sy > 0) {
+    todo.push({
+      set: newset,
+      method: "basic",
+      iterator: iterate_basic
+    });
+  }
 
   newset = set.intersect(-0.75, 1.2, null, 0.75);
   if (newset.image.sy > 0 && newset.image.sx > 0) {
